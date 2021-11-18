9a576ab6b19b1c016ab30e8604c8c340ed68936e
Valentine520 <89323617+Valentine520@users.noreply.github.com>
2021-11-18 10:20:13 +0800

    Update Heap.c

--- Heap/Heap/Heap.c
+++ Heap/Heap/Heap.c
@@ -8,48 +8,43 @@
 
 static void swap(Item*data1,Item*data2);//交换元素 辅助
 
-void Adjast(Heap*heap,int index);//调整堆
 
-//在堆中插入元素
+Heap* Initial(Heap*heap){
+    //为heap分配内存空间
+    heap=(Heap*)malloc(sizeof(Heap));
+    if(heap==NULL){
+        fprintf(stderr, "Fail to allocate the memory\n");
+        return heap;
+    }
+    heap->size=0;
+    heap->element=(Element*)malloc(sizeof(Element)*MAXSIZE+1);
+    heap->capacity=MAXSIZE;//头节点作为哨兵使用
+    heap->element[0].data=MAXSIZE;//最大堆
+    return heap;
+}
+
+
+//在堆中插入元素 percolate up
 void Insert(Heap*heap,Item data){
-    //寻找插入的位置
-    int index=heap->size+1;//下标从1开始 易于index
-    while(index>0){
-        //判断何处可以插入
-        //表示pre节点的内容
-        if(heap->List[index/2].data<data){
-            heap->List[index]=heap->List[index/2];//节点下移
-        }
-        else{
-            break;
-        }
-        index=index/2;//向pre节点
+    if(heap->size>=heap->capacity){
+        fprintf(stderr,"The heap is full.\n");
+        return;
     }
-    heap->List[index].data=data;
-    heap->size++;
+    PercolateUp(heap,heap->size+1,data);//从插入位置开始向上调整
+    heap->size++;//增加元素个数 代表插入
 }
 
-//删除元素一定是从堆顶删除 堆顶元素和堆底元素对掉
+//删除元素一定是从堆顶删除 堆顶元素和堆底元素交换 然后再删除 3p型交换 不方便 可以保存节点的值作为参数传入
 void Delete(Heap*heap){
-    heap->List[1].data=heap->List[heap->size].data;
-    heap->size--;
+    //不使用值交换的方式 减少内存开销
+    if(heap->size==0){
+        fprintf(stderr,"The heap is empty.\n");
+        return;
+    }
+    int data=heap->element[heap->size].data;
     //开始调整
-    int index=1;
-    Adjast(heap,index);
-}
-
-
-//创建堆
-void Create(Heap*heap,const Item *Data,int size){
-    //先直接插入 而后调整 从第一个含根的节点开始调整 向下调整
-    for(int i=1;i<=size;i++){
-        heap->List[i].data=Data[i-1];//直接拷贝
-    }
-    heap->size=size;
-    int index=size/2;//第一个含根的亲节点
-    for(int i=index;i>=1;i--){
-        Adjast(heap,i);
-    }
+    PercolateDown(heap,1,data);//从堆顶开始向下调整
+    heap->size--;//减少元素个数 代表删除
 }
 
 
@@ -59,42 +54,150 @@ void swap(Item*data1,Item*data2){
     *data2=temp;
 }
 
-void Adjast(Heap*heap,int index){
-    //向下调整
-    while(index*2<=heap->size){
-        //比较左边 如果左child 不存在那么就无需调整了
-        if(index*2+1<=heap->size) {
-            //看右边是否存在
-            //右边存在比较左边和右边的大小 右边大
-            if (heap->List[index * 2].data < heap->List[index * 2 + 1].data &&
-                heap->List[index * 2 + 1].data > heap->List[index].data) {
-                //和右边交换
-                swap(&heap->List[index].data, &heap->List[index * 2 + 1].data);
-                index = index * 2 + 1;//向右
-            } else if (heap->List[index * 2].data > heap->List[index * 2 + 1].data &&
-                       heap->List[index * 2].data > heap->List[index].data) {
-                //和左边交换
-                swap(&heap->List[index].data, &heap->List[index * 2].data);
-                index *= 2;//向左
-            } else {
-                break;//无须调整
-            }
+//需要加入边界判断值 删除前是否为空 为空则无法删除  percolate down
+void PercolateDown(Heap*heap,int index,int data){
+    int child;
+    int i;
+    for(i=index;i*2<=heap->size;i=child){
+        child=i*2;//i的child
+        if(child!=heap->size&&heap->element[child].data<heap->element[child+1].data){
+            child++;//右侧节点存在且大于左侧 则更新child为待移动节点
         }
-        else if(heap->List[index].data<heap->List[index*2].data){
-                swap(&heap->List[index].data,&heap->List[index*2].data);
-                break;
-            }
-        //均没有比其大的
+        if(heap->element[child].data>data){
+            heap->element[i].data=heap->element[child].data;
+        }
         else{
             break;
         }
-
     }
+    //确保for外部就是待插入的位置
+    heap->element[i].data=data;
 }
+
+void PercolateUp(Heap*heap,int index,int data){
+    //Heap的下标从1开始编号 易于查找index
+    //避免到头 使用MAXSIZE放在0号位置 则一定会结束循环 不可能比0号位更大 
+    while(1){
+        //判断何处可以插入
+        //表示pre节点的内容 最大堆 插入的位置应该满足其最大堆的性质
+        if(heap->element[index/2].data<data){
+            heap->element[index]=heap->element[index/2];//节点下移
+        }
+        else{
+            break;
+        }
+        index=index/2;//向pre节点
+    }
+    heap->element[index].data=data;//index即为查找到安放节点的位置
+
+}
+
+
 
 void heap_traverse(Heap*heap){
     for(int i=1;i<=heap->size;i++){
-        printf("%d ",heap->List[i].data);
+        printf("%d ",heap->element[i].data);
     }
     printf("\n");
+}
+
+
+Item GetTop(Heap*heap){
+    //返回堆顶的元素
+    if(heap->size==0){
+        fprintf(stderr,"The heap is empty\n");
+        return 0;
+    }
+    return heap->element[1].data;//堆顶元素在首位
+}
+
+//Locate the data in heap
+int LocateKey(Heap*heap,int data){
+    int site=-1;
+    if(heap->size==0){
+        site=-1;
+        return site;
+    }
+    //有child 只能通过遍历的方式查找值
+    for(int i=0;i<heap->size;i++){
+        if(heap->element[i].data==data){
+            site=i;
+            return site;
+        }
+    }
+    return site;//If the node is not exists in the heap
+}
+
+
+//Decrease the value of key
+bool DecreaseKey(Heap *heap,int data,int new_value){
+    //查找该值
+    int site=LocateKey(heap, data);
+    if(site==-1){
+        fputs("The value is not exists",stderr);
+        return 0;
+    }
+    else{
+        //将该值赋予新的值过后向下调整 因为是最大堆
+        PercolateDown(heap,site,new_value);//从新减少值的点向下调整 如果为最小堆的相反
+    }
+    return 1;
+}
+
+
+//Increase the value of key
+bool IncreaseKey(Heap *heap,int data,int new_value){
+    //查找该值
+    int site=LocateKey(heap, data);
+    if(site==-1){
+        fputs("The value is not exists",stderr);
+        return 0;
+    }
+    else{
+        //将该值赋予新的值过后向上调整 因为是最大堆
+        PercolateUp(heap,site,new_value);//从新增加值的点向上调整 如果为最小堆的相反
+    }
+    return 1;
+}
+
+
+
+//Delete the key node of the heap
+bool DeleteKey(Heap *heap,int data){
+    //升高它的优先级 升至堆顶
+    if(!IncreaseKey(heap,data,heap->element[1].data+1)){
+        return 0;
+    }
+    //删除堆顶元素 则实现该节点的删除
+    Delete(heap);
+    return 1;
+}
+
+//Percolate down the every root node
+Heap* BuildHeap(const Item *Data,int size){
+    Heap *heap;
+    //先构建Heap
+    heap=Initial(heap);
+    for(int i=0;i<size;i++){
+        heap->element[i+1].data=Data[i];//按照顺序拷贝即可
+    }
+    heap->size=size;
+    //开始调整节点 从含有child的首个root开始
+    for(int i=heap->size/2;i>=1;i--){
+        PercolateDown(heap,i,heap->element[i].data);//以该处的元素下滤 类似于插入排序
+    }
+    return heap;
+}
+
+Item Find(const Item *Data,int size,int k){
+    //以k建堆
+    Heap*heap=BuildHeap(Data,k);//建堆
+    //循环比对
+    for(int i=k;i<size;i++){
+        if(heap->element[i-1].data<Data[i]){
+            //以新元素上滤
+            PercolateDown(heap,k,Data[i]);
+        }
+    }
+    return heap->element[heap->size].data;//末尾元素即是堆
 }
