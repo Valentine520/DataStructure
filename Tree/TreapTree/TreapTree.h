279fd2dda56ce9b542f3094f4273acf6a75e8929
Valentine520 <89323617+Valentine520@users.noreply.github.com>
2021-11-20 01:50:43 +0800

    Update TreapTree.h

--- Tree/TreapTree/TreapTree.h
+++ Tree/TreapTree/TreapTree.h
@@ -15,6 +15,8 @@
  * 结合Minimize Heap 和 Binary Search Tree 的思想完成Treap tree的构造
  * 其中很多操作在Heap和BST中都可见
  * 例如percolate up 在Heap的insert时常用
+ * 以及percolate down 在Heap的delete时常用
+ * delete在众多算法中都是比较复杂的 本数据结构中也不例外
  * 再比如TransPlant 在 Binary Search tree 的Delete中常用
  * 以及Successor Traverse Search等操作
  */
@@ -32,8 +34,8 @@ typedef struct TreapTree{
 //插入节点
 Treap Insert(Treap tree,int data);
 
-//删除
-bool Delete(Treap *tree,int data);
+//采用Treap节点旋转调整法删除
+Treap Delete(Treap Tree,int data);
 
 //查找
 Treap Search(Treap tree,int data);
@@ -46,6 +48,13 @@ Treap LRotate(Treap node);
 
 //从下向上调整priority
 Treap PercolateUp(Treap tree,Treap node);
+
+//从上到下调整 在delete中使用
+Treap PercolateDown(Treap Tree,Treap node);
+
+//返回节点的child的最高优先级 不存在左右node则返回-1
+//左高为1 右高为2
+int Priority(Treap node);
 
 //遍历二叉树
 void Traverse(Treap tree);
