2ec8ac22f7ad8c63bf15820ca8c1c0012757978e
Valentine520 <89323617+Valentine520@users.noreply.github.com>
2021-11-20 01:50:19 +0800

    Update TreapTree.c

--- Tree/TreapTree/TreapTree.c
+++ Tree/TreapTree/TreapTree.c
@@ -61,7 +61,6 @@ Treap PercolateUp(Treap tree,Treap node){
                     else{
                         temp=node->P->P;
                         temp->Rchild=RRotate(node);
-
                     }
                 }
                 else{
@@ -80,7 +79,6 @@ Treap PercolateUp(Treap tree,Treap node){
                     else{
                         temp=node->P->P;
                         temp->Rchild= LRotate(node);
-
                     }
                 }
                 else{
@@ -95,48 +93,106 @@ Treap PercolateUp(Treap tree,Treap node){
     return tree;
 }
 
-////删除
-bool Delete(Treap *tree,int data){
-    //简易版 直接交换priority
-    Treap temp=*tree;
+Treap PercolateDown(Treap Tree,Treap node){
+    Treap Temp=Tree;
+    if(node==Tree){
+        //查找priority最高(此处最小则为最高)的节点 更新根节点的指向
+        if(Tree->Lchild!=NULL&&Tree->Rchild!=NULL){
+            //左侧节点的优先级更高
+            if(Tree->Lchild->priority<Tree->Rchild->priority){
+                //右旋
+                Tree= RRotate(node->Lchild);//右旋
+            }
+            else{
+                Tree= LRotate(node->Rchild);//左旋
+            }
+        }
+        else if(Tree->Lchild!=NULL){
+            Tree= RRotate(node->Lchild);
+        }
+        else{
+            Tree= LRotate(node->Rchild);
+        }
+    }
+    //为根则修改Tree的指向 同时node也已经被更新 此时直接执行非Root的删除即可
+    //查找左右两侧节点的最高优先级
+    int Suc= Priority(node);
+    Treap pre;
+    while (Suc!=-1){
+        pre=node->P;
+        switch (Suc) {
+            case 1:
+                //左优先级高 则右旋
+                //使根节点和旋转后的节点相连
+                if(pre->Lchild==node){
+                    pre->Lchild= RRotate(node->Lchild);
+                }
+                else{
+                    pre->Rchild= RRotate(node->Lchild);
+                }
+                break;
+            case 2:
+                if(pre->Lchild==node){
+                    pre->Lchild= LRotate(node->Rchild);
+                }
+                else{
+                    pre->Rchild= LRotate(node->Rchild);
+                }
+                break;
+            default:
+                fprintf(stderr,"error\n");
+        }
+        Suc= Priority(node);
+    }
+    pre=node->P;
+    //直到node变成了叶节点
+    if(pre->Lchild==node){
+        pre->Lchild=NULL;
+    }
+    else{
+        pre->Rchild=NULL;
+    }
+    free(node);
+    return Tree;
+}
+
+int Priority(Treap node){
+    int flag;
+    //两侧都为NULL则直接删除
+    if(node->Lchild==NULL&&node->Rchild==NULL){
+        return -1;//表示为叶节点 可以直接删除
+    }
+    if(node->Lchild==NULL){
+        flag =2;//右侧大
+    }
+    else if(node->Rchild==NULL){
+        flag=1;//左侧大
+    }
+    else{
+        if(node->Lchild->priority<node->Rchild->priority){
+            flag=1;//左侧优先级根高
+        }
+        else{
+            flag=2;
+        }
+    }
+    return flag;
+}
+
+Treap Delete(Treap Tree,int data){
+    Treap temp = Tree;
     Treap node= Search(temp,data);
     int flag=0;
     if(node==NULL){
         fprintf(stderr,"The node is not exist.\n");
-        return flag;
+        return NULL;
     }
-    flag=1;
-    //分三种情况删除 Lchild不存在
-    if(node->Lchild==NULL){
-        TransPlant(tree,node,node->Rchild);
-        return flag;
-    }
-    else if(node->Rchild==NULL){
-        TransPlant(tree,node,node->Lchild);
-        return flag;
-    }
-    //查找successor
-    Treap suc= Successor(node);
-    //表明无需调整
-    if(suc==node->Rchild){
-        suc->priority=node->value;
-        TransPlant(tree,node,node->Rchild);
-    }
-    else{
-        TransPlant(tree,suc,suc->Rchild);
-        suc->Rchild=node->Rchild;
-        if(node->Rchild!=NULL){
-            node->Rchild->P=suc;
-        }
-        suc->priority=node->priority;
-        suc->Lchild=node->Lchild;
-        if(node->Lchild!=NULL){
-            node->Lchild->P=suc;
-        }
-        TransPlant(tree,node,suc);
-    }
-    return flag;
+    //查找到该元素 从该节点下滤
+    Tree= PercolateDown(Tree,node);
+    return Tree;
 }
+
+
 
 //查找 二叉树的搜索
 Treap Search(Treap tree,int data){
