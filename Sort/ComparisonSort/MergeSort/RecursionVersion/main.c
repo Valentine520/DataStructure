52b66495c1072e31be8a08c1fa7fabb2bc889416
Valentine520 <89323617+Valentine520@users.noreply.github.com>
2021-12-01 09:24:21 +0800

    Create main.c

---
+++ Sort/ComparisonSort/MergeSort/RecursionVersion/main.c
@@ -0,0 +1,15 @@
+#include "MergeSort_R.h"
+
+int main(void){
+    int array[]={1,2,3,1};
+    int size=sizeof(array)/sizeof(array[0]);
+    for(int i=0;i<size;i++){
+        printf("%d ",array[i]);
+    }
+    putchar('\n');
+    MergeSort(array,size);
+    for(int i=0;i<size;i++){
+        printf("%d ",array[i]);
+    }
+    return 0;
+}
